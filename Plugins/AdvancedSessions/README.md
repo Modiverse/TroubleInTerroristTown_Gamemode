ea5eab7c35ad492ed3f2c188e98f72df22a97cbe
KillerJim-DVG <JamesHunter-DeepVoodoo@users.noreply.github.com>
2020-10-21 16:49:23 +0100

    Initial Commit

---
+++ Plugins/AdvancedSessions/README.md
@@ -0,0 +1,7 @@
+### How do I use it? ###
+
+**KantanDocGen Automatic Documentation ([KantanDocGen](http://kantandev.com/free/kantan-doc-gen))** 
+
+**[AdvancedSessions](https://mordentral.bitbucket.io/AdvancedSessions/Advanced)**
+
+**[AdvancedSteamSessions](https://mordentral.bitbucket.io/AdvancedSteamSessions/Advanced)**
---
+++ copyPlugin.bat
@@ -0,0 +1,3 @@
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll D:\DVG\KJModGM_TTT\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll D:\DVG\KJModGM_TTT\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll
+timeout 2
